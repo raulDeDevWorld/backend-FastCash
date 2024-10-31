a89803d495eec2042574418a97ea5e446dc71d18
Your Name <you@example.com>
2024-10-31 19:12:00 -0400

    DC

--- api/index.js
+++ api/index.js
@@ -73,5 +73,5 @@ app.delete('/api/data/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
